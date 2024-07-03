3a7a42de48a0545637bca7d9c33ca785636ffdb6
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-07-03 14:44:34 +0100

    Feat: Basic Player Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Components")]
     private IA_PlayerControls playerControls;
+    private CharacterController controller;
 
-    [SerializeField] private Vector2 moveInput;
-    [SerializeField] private Vector2 aimInput;
+    [Header("Player Inputs")]
+    private Vector2 moveInput;
+    private Vector2 aimInput;
+    private Vector3 movementDirection;
+    
+    [Header("Movement Settings")]
+    [SerializeField] private float MovementSpeed = 5f;
 
     private void Awake()
     {
         playerControls = new IA_PlayerControls();
+        
+        controller = GetComponent<CharacterController>();
 
         playerControls.Character.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         playerControls.Character.Movement.canceled += ctx => moveInput = Vector2.zero;
@@ -21,7 +30,12 @@ public class PlayerMovement : MonoBehaviour
         playerControls.Character.Aim.performed += ctx => aimInput = ctx.ReadValue<Vector2>();
         playerControls.Character.Aim.canceled += ctx => aimInput = Vector2.zero;
     }
-    
+
+    private void Update()
+    {
+        Move();
+    }
+
     private void OnEnable()
     {
         playerControls.Enable();
@@ -30,5 +44,14 @@ public class PlayerMovement : MonoBehaviour
     private void OnDisable()
     {
         playerControls.Disable();
+    }
+
+    private void Move()
+    {
+        movementDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+        if (movementDirection.magnitude > 0)
+        {
+            controller.Move(movementDirection * (MovementSpeed * Time.deltaTime));
+        }
     }
 }
